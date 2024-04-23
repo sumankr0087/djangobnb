5cd769f13d4e346c1be02f750d68308475ef9f1f
sumankr0087 <suman.kumar@essentia.dev>
2024-04-23 05:58:04 +0530

    property list api

--- frontend/app/components/properties/PropertyList.tsx
+++ frontend/app/components/properties/PropertyList.tsx
@@ -1,11 +1,41 @@
+'use client'
+import { useEffect, useState } from 'react'
 import React from 'react'
 import PropertyListItem from './PropertyListItem'
+
+export type PropertyType = {
+  id: string;
+  title: string;
+  price_per_nigth: number;
+  image_url: string
+}
+
 function PropertyList() {
+  const [properties, setProperties] = useState<PropertyType[]>([])
+  const getProperties = async () => {
+    const url = 'http://localhost:8000/api/properties/'
+    await fetch(url, {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then((json) => {
+      console.log('json', json);
+      setProperties(json.data)
+    })
+    .catch((error) => {console.log('error', error);
+    })
+  }
+  useEffect(() => {
+    getProperties();
+  }, [])
   return (
     <div className="mt-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
-      <PropertyListItem/>
-      <PropertyListItem/>
-      <PropertyListItem/>
+      {properties.map((property) => {
+        return (
+      <PropertyListItem key={property.id}
+      property={property} />
+        )
+      })}
     </div>
   )
 }
--- frontend/app/components/properties/PropertyListItem.tsx
+++ frontend/app/components/properties/PropertyListItem.tsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import Image from 'next/image'
+import { PropertyType } from './PropertyList'
 
-function PropertyListItem() {
+interface PropertyProps {
+  property : PropertyType
+}
+const PropertyListItem: React.FC<PropertyProps>=({property}) => {
   return (
     <div className='cursor-pointer'>
       <div className='relative overflow-hidden aspect-square rounded-xl'>
         <Image
         fill
-        src='/beachhouse.jpeg'
-        sizes='(max-width:768px) 768px, (max-width: 1200px): 768px, 768px'
+        src={property.image_url}
+        // sizes='(max-width:768px) 768px, (max-width: 1200px): 768px, 768px'
         className='hover:scale-110 object-cover transition h-full w-full'
         alt="beach house" />
       </div>
 
       <div className='mt-2'>
-        <p className='text-lg font-bold'> property name</p>
+        <p className='text-lg font-bold'>{property.title}</p>
       </div>
       <div className='mt-2'>
-        <p className='text-sm text-gray-700'><strong>$200</strong> per night</p>
+        <p className='text-sm text-gray-700'><strong>${property.price_per_nigth}</strong> per night</p>
       </div>
     </div>
   )
